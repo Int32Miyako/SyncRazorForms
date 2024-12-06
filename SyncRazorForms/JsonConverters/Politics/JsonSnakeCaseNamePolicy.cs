afb6c17e8b028c688ffeb5fd88e0ee94393d71d2
Int32Miyako <bogdan47.2006@gmail.com>
2024-12-06 17:02:47 +0300

    json

---
+++ SyncRazorForms/JsonConverters/Politics/JsonSnakeCaseNamePolicy.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace SyncRazorForms.JsonConverters.Politics;
+
+public class JsonSnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+         
+        var chars = name.ToCharArray();
+        var newString = new List<char>();
+        
+        foreach (var symbol in chars)
+        {
+            if (char.IsUpper(symbol))
+            {
+                
+                newString.Add('_');
+            }
+            
+            newString.Add(char.ToLower(symbol));
+        }
+        
+        return string.Join("", newString);
+    }
+}
